4d230016ba4b4d83c2398e37f65c5636a29f2f7e
ShakilAhmed007 <sakilhossain01969@gmail.com>
2021-07-09 20:40:13 +0600

    ➕ Add: Tailwind, React, Inertiajs added

---
+++ resources/js/Pages/welcome.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function welcome() {
+    return (
+        <div>
+            this is my home page!
+        </div>
+    )
+}
+
+export default welcome
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,6 +3,9 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
         <title>Laravel</title>
 
@@ -21,6 +24,7 @@
         </style>
     </head>
     <body class="antialiased">
+        <button class="bg-gray-700">hello</button>
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
             @if (Route::has('login'))
                 <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
