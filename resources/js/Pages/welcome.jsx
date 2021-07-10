58d18ef1a81ac2360645ed7c684a4859a57a607b
ShakilAhmed007 <sakilhossain01969@gmail.com>
2021-07-10 11:17:02 +0600

    📄 Add: .prettierrc and some editor configs

--- resources/js/Pages/welcome.jsx
+++ resources/js/Pages/welcome.jsx
@@ -1,18 +1,22 @@
-import React from 'react'
+import React from "react";
 
 function welcome() {
-    return (
-        <div>
-            <div className="">
-                <div className="card text-center bg-gray-600 flex items-center justify-center h-screen">
-                    <div className='text-white inline-block '>
-                    <h1 className='font-bold text-6xl'>Laravel<span className='text-red-600'>+</span>React<span className='text-red-600'>+</span>Inertia<span className='text-red-600'>+</span>Tailwind</h1>
-                    <h2 className='font-bold text-4xl'>Starter kit</h2>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div>
+      <div className="">
+        <div className="card text-center bg-gray-600 flex items-center justify-center h-screen">
+          <div className="text-white inline-block ">
+            <h1 className="font-bold text-6xl">
+              Laravel<span className="text-red-600">+</span>React
+              <span className="text-red-600">+</span>Inertia
+              <span className="text-red-600">+</span>Tailwind
+            </h1>
+            <h2 className="font-bold text-4xl">Starter kit</h2>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-export default welcome
+export default welcome;
